cd9759b8f0c5350f453c7a9484558b126b5dd40e
Tiree Jackson <tiree_jackson@yahoo.com>
2021-01-04 23:47:53 -0500

    not finish the buttons keep getting a error

--- ghostpost/src/App.js
+++ ghostpost/src/App.js
@@ -1,25 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      ghostPost: [],
+    };
+  }
+
+  totalVotes() {
+    fetch("http://127.0.0.1:8000/api/post/total/")
+      .then((res) => res.json())
+      .then((data) => this.setState({ ghostPost: data }));
+  }
+  boasts() {
+    fetch("http://127.0.0.1:8000/api/post/boasts/")
+      .then((res) => res.json())
+      .then((data) => this.setState({ ghostPost: data }));
+  }
+  roasts() {
+    fetch("http://127.0.0.1:8000/api/post/roasts/")
+      .then((res) => res.json())
+      .then((data) => this.setState({ ghostPost: data }));
+  }
+  upVote(id) {
+    fetch(`http://127.0.0.1:8000/api/post/${id}/up_vote/`, { method: "GET" });
+    window.location.reload();
+  }
+  downVote(id) {
+    fetch(`http://127.0.0.1:8000/api/post/${id}/down_vote/`, { method: "GET" });
+    window.location.reload();
+  }
+  componentDidMount() {
+    fetch("http://localhost:8000/api/post")
+      .then((res) => res.json())
+      .then((data) => this.setState({ ghostPost: data }));
+  }
+
+  render() {
+    return (
+      <div>
+        <button onClick={() => this.boasts()}>Boasts</button>
+        <button onClick={() => this.roasts()}>Roasts</button>
+        <button onClick={() => this.totalVotes()}>Total Votes</button>
+        <form id="postForm" onSubmit={this.handleSubmit}>
+          <h3>
+            User Input:
+            <input id="userinput" name="input" type="text" />
+          </h3>
+
+          <button>Create Post</button>
+        </form>
+        <hr class="rounded"></hr>
+        {this.state.ghostPost.map((p) => (
+          <li>
+            PostType:{p.post_type} Content:{p.content} DateCreated:
+            {p.date_created} TotalVotes:{p.total_votes}
+            <button onClick={() => this.upVote(p.id)}>Upvote</button>
+            <button onClick={() => this.downVote(p.id)}>Downvote</button>
+          </li>
+        ))}
+      </div>
+    );
+  }
 }
-
 export default App;
